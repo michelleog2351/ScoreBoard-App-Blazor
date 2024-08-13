9841c0bc46b444bfc6e0a06a2d7649a81d994fe2
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-08-14 00:41:00 +0100

    BugFix to input boxes, align to centre and add input boxes for the scores

--- ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
+++ ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f76d3880e0b86687dae94c58046a75b0ccb2b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546fe24a0001b64a4b955ae8b7507e46a25c58f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
