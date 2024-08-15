1af8edabfa8e8e628a2ad57c6ecaaad647d81d5c
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-08-16 00:50:40 +0100

    add timer variable, bugfix needed to make sure iterms are in line

--- ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
+++ ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9841c0bc46b444bfc6e0a06a2d7649a81d994fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e95ce17af00f6698d60346b956f0f519026a1ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
