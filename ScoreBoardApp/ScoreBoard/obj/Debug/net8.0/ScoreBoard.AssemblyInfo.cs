75aef77da9279cf365c9508e91c9ebffd8c25f33
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-08-17 00:15:20 +0100

    Add more logic to timer code and ass table to store past scores

--- ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
+++ ScoreBoardApp/ScoreBoard/obj/Debug/net8.0/ScoreBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e95ce17af00f6698d60346b956f0f519026a1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af8edabfa8e8e628a2ad57c6ecaaad647d81d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScoreBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
